508b7fa1f52c94dc5f834600e0478895e46d2952
Arseniy Khmelevoy <kekcij@mail.ru>
2023-05-30 13:59:01 +0300

    Remove extra JS code

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,24 +1,5 @@
 const dropdownList = document.querySelectorAll('.dropdown')
 
-// dropdownList.forEach(function (element) {
-//     element.addEventListener('mouseover', function (evt) {
-//         let alreadyActive = document.querySelectorAll('.dropdown.active')
-//         for (let index = 0; index < alreadyActive.length; index++) {
-//             if (alreadyActive[index] === element) {
-//                 continue
-//             }
-//             alreadyActive[index].classList.remove('active')
-//         }
-//         element.classList.toggle('active')
-//     })
-// })
-
-
-// dropdownList.forEach(function (element) {
-//     element.addEventListener('mouseout', function (evt) {
-//         element.classList.toggle('active')
-//     })
-// })
 
 function disableDropdowns (element) {
     let alreadyActive = document.querySelectorAll('.dropdown.active')
@@ -30,11 +11,13 @@ function disableDropdowns (element) {
     }
 }
 
+
 dropdownList.forEach(function (element) {
     element.addEventListener('mouseover', function (evt) {
         disableDropdowns(element)
     })
 })
+
 
 dropdownList.forEach(function (element) {
     element.addEventListener('click', function (evt) {
